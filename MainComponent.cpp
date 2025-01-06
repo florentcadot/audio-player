55ad0547acd16acfeed2ff1b0798d8d28d8e915c
florentcadot <florentcadot.dev@gmail.com>
2025-01-06 23:00:52 +0100

    [WIP] open file and change state

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -19,6 +19,9 @@ MainComponent::MainComponent()
         stopButton.setEnabled (false);
         addAndMakeVisible (&stopButton);
 
+        formatManager.registerBasicFormats(); 
+        transportSource.addChangeListener(this);
+
         setSize (600, 400);
 }
 
@@ -27,7 +30,6 @@ void MainComponent::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
     g.setFont (juce::FontOptions (16.0f));
     g.setColour (juce::Colours::white);
 }
@@ -40,4 +42,106 @@ void MainComponent::resized()
     openButton.setBounds(10, 10, getWidth() - 20, 30);
     playButton.setBounds(10, 50, getWidth() - 20, 30);
     stopButton.setBounds(10, 90, getWidth() - 20, 30);
+}
+
+void MainComponent::openButtonClicked()
+{
+     std::unique_ptr <juce::FileChooser> chooser = std::make_unique<juce::FileChooser> ("Select a Wave file to play...",
+                                                    juce::File{},
+                                                    "*.wav");                     // [7]
+    auto chooserFlags = juce::FileBrowserComponent::openMode
+                        | juce::FileBrowserComponent::canSelectFiles;
+
+    chooser->launchAsync (chooserFlags, [this] (const juce::FileChooser& fc)     // [8]
+    {
+        auto file = fc.getResult();
+
+        if (file != juce::File{})                                                // [9]
+        {
+            auto* reader = formatManager.createReaderFor (file);                 // [10]
+
+            if (reader != nullptr)
+            {
+                auto newSource = std::make_unique<juce::AudioFormatReaderSource> (reader, true);   // [11]
+                transportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);       // [12]
+                playButton.setEnabled (true);                                                      // [13]
+                readerSource.reset (newSource.release());                                          // [14]
+            }
+        }
+    });
+}
+
+void MainComponent::playButtonClicked()
+{
+    DBG("playButtonClicked");
+}
+
+void MainComponent::stopButtonClicked()
+{
+    DBG("stopButtonClicked");
+}
+
+void MainComponent::changeState(TransportState newState) {
+    if (state != newState)
+    {
+        state = newState;
+        switch(state)
+        {
+            case Stopped:
+                stopButton.setEnabled(false);
+                playButton.setEnabled(true);
+                transportSource.setPosition (0.0);
+                break;
+            
+            case Starting:
+                playButton.setEnabled(true);
+                transportSource.start();
+                break;
+        
+            case Playing:                           
+                stopButton.setEnabled (true);
+                break;
+
+            case Stopping:                         
+                transportSource.stop();
+                break;
+        }
+    }
+};
+
+void MainComponent::changeListenerCallback (juce::ChangeBroadcaster* source){
+        if (source == &transportSource)
+        {
+        if (transportSource.isPlaying()) 
+        {
+            changeState(Playing);
+        }
+        else {
+            changeState (Stopped);
+        };
+        };
+};
+
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    if (readerSource.get() == nullptr)
+    {
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void MainComponent::releaseResources()
+{
+    transportSource.releaseResources();
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
 }
--- MainComponent.h
+++ MainComponent.h
@@ -4,29 +4,27 @@
 // directly. If you need to remain compatible with Projucer-generated builds, and
 // have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
 // you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
-#include <juce_gui_extra/juce_gui_extra.h>
 
+
+#include <juce_gui_extra/juce_gui_extra.h>
+#include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_audio_basics/juce_audio_basics.h>
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent final : public juce::Component
+
+class MainComponent final : public juce::AudioAppComponent,
+                            public juce::ChangeListener
+                            
 {
 public:
     MainComponent();
     void paint (juce::Graphics&) override;
     void resized() override;
-
-    void openButtonClicked() {
-        DBG("Open Button clicked");
-    };
-    void playButtonClicked() {
-        DBG("Play Button clicked");
-    };
-        void stopButtonClicked() {
-        DBG("Stop Button clicked");
-    };
 
 private:
     enum TransportState
@@ -40,6 +38,19 @@ private:
     juce::TextButton openButton;
     juce::TextButton playButton;
     juce::TextButton stopButton;
+    void openButtonClicked(); 
+    void playButtonClicked();
+    void stopButtonClicked();
+
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+    void changeListenerCallback (juce::ChangeBroadcaster* source) override;
+    void changeState(TransportState newState);
+
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
